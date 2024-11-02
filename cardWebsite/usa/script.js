a2426bdaf2ed5078f2036522b7ce9eca65de2357
Harsh <kaushikyuraj134@gmail.com>
2024-11-02 17:36:39 +0530

    added animation to timeline

--- cardWebsite/usa/script.js
+++ cardWebsite/usa/script.js
@@ -1,30 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const timelineEvents = document.querySelectorAll('.timeline-event');
-    const prevButton = document.querySelector('.timeline-button.prev');
-    const nextButton = document.querySelector('.timeline-button.next');
-    let currentIndex = 0;
-
-    function updateTimeline() {
-        timelineEvents.forEach((event, index) => {
-            event.classList.remove('active');
-            if (index === currentIndex) {
-                event.classList.add('active');
-            }
-        });
-    }
-
-    prevButton.addEventListener('click', () => {
-        currentIndex = (currentIndex > 0) ? currentIndex - 1 : timelineEvents.length - 1;
-        updateTimeline();
-    });
-
-    nextButton.addEventListener('click', () => {
-        currentIndex = (currentIndex < timelineEvents.length - 1) ? currentIndex + 1 : 0;
-        updateTimeline();
-    });
-
-    updateTimeline();
+ 
 });
+
+/**********************Scroll Animation "START"************************************/
+$(document).ready(function(){
+    var $animation_elements = $('.anim');
+    var $window = $(window);
+    
+    function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+    
+    $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+    
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+    (element_top_position <= window_bottom_position)) {
+    $element.addClass('animated');
+    } else {
+    $element.removeClass('animated');
+    }
+    });
+    }
+    
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+    });
+    /**********************Scroll Animation "END"************************************/
+    
+    /**********************Change color of center aligned animated content small Circle  "START"************************************/
+    $(document).ready(function(){
+        $(" .debits").hover(function(){
+            $(" .center-right").css("background-color", "#4997cd");
+            }, function(){
+            $(" .center-right").css("background-color", "#fff");
+        }); 
+    });
+    $(document).ready(function(){
+        $(".credits").hover(function(){
+            $(".center-left").css("background-color", "#4997cd");
+            }, function(){
+            $(".center-left").css("background-color", "#fff");
+        }); 
+    });
+    /**********************Change color of center aligned animated content small Circle  "END"************************************/
 
 // Example flight API using AviationStack (free tier)
 const flightApiKey = 'df31bec6525fc9a841a458916a37131e';
